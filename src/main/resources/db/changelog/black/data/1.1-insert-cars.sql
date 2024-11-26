aac6ac91d153c3bf3a1e7b5ce915f8cfb48b8f24
sproshchaev <sproshchaev@gmail.com>
2024-11-26 22:17:53 +0500

    add profiles black & white

---
+++ src/main/resources/db/changelog/black/data/1.1-insert-cars.sql
@@ -0,0 +1,2 @@
+-- Insert initial cars
+INSERT INTO car (id, brand, color) VALUES (1, 'BMW', 'black');
--- src/main/resources/db/changelog/data/1.1-insert-cars.sql
+++
@@ -1,3 +0,0 @@
--- Insert initial cars
-INSERT INTO car (id, brand, color) VALUES (1, 'BMW', 'black');
-INSERT INTO car (id, brand, color) VALUES (2, 'BMW', 'white');
