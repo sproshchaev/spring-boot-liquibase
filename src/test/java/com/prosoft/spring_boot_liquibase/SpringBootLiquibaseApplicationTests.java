f02b408944c69570282917c5f2ff47f5787c73ca
sproshchaev <sproshchaev@gmail.com>
2024-11-26 21:01:19 +0500

    init from start.spring.io

---
+++ src/test/java/com/prosoft/spring_boot_liquibase/SpringBootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosoft.spring_boot_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
