05067b8c5e00353cd676752d9258d19d6e323438
sproshchaev <sproshchaev@gmail.com>
2024-11-27 17:19:07 +0500

    postgresql ready

--- src/main/java/com/prosoft/spring_boot_liquibase/SpringBootLiquibaseApplication.java
+++ src/main/java/com/prosoft/spring_boot_liquibase/SpringBootLiquibaseApplication.java
@@ -5,21 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
-public class SpringBootLiquibaseApplication { //implements CommandLineRunner {
+@EnableJdbcRepositories
+@ComponentScan(basePackages = "com.prosoft.spring_boot_liquibase")
+public class SpringBootLiquibaseApplication implements CommandLineRunner {
 
-    //@Autowired
-    //private CarRepository carRepository;
+    @Autowired
+    private CarRepository carRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLiquibaseApplication.class, args);
     }
 
-//    @Override
-//    public void run(String... args) throws Exception {
-//        System.out.println("Spring Boot Liquibase started");
-//        System.out.println("Cars: " + carRepository.findAll());
-//    }
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Spring Boot Liquibase started");
+        System.out.println("Cars: " + carRepository.findAll());
+    }
 }
