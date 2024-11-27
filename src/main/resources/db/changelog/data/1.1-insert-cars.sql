f6539ba5c90994625694d5d9f2ce7cadeac0d247
sproshchaev <sproshchaev@gmail.com>
2024-11-27 15:40:38 +0500

    fix

--- src/main/resources/db/changelog/data/1.1-insert-cars.sql
+++ src/main/resources/db/changelog/data/1.1-insert-cars.sql
@@ -1,3 +1,2 @@
--- Insert initial cars
-INSERT INTO car (id, brand, color) VALUES (1, 'BMW', 'black');
-INSERT INTO car (id, brand, color) VALUES (2, 'BMW', 'white');
+INSERT INTO car (brand, color) VALUES ('BMW', 'black');
+INSERT INTO car (brand, color) VALUES ('BMW', 'white');
--- src/main/resources/db/changelog/structure/1.0-create-car-table.sql
+++ src/main/resources/db/changelog/structure/1.0-create-car-table.sql
@@ -1,4 +1,4 @@
--- Create table `car`
+
 CREATE TABLE car (
                      id BIGINT PRIMARY KEY AUTO_INCREMENT,
                      brand VARCHAR(255) NOT NULL,
